c39de8f1b760d05b94ea5badeb3b7ca2306459c1
jpbarto <jason.p.barto@gmail.com>
2022-08-14 14:58:28 +0100

    added admin permissions back to the container

--- deploy/docker-compose/deploy.sh
+++ deploy/docker-compose/deploy.sh
@@ -7,28 +7,30 @@ ECS_DOCKER_CONTEXT=awscluster
 REGION=${AWS_DEFAULT_REGION}
 ACCT_NO=$(aws sts get-caller-identity --query 'Account' --output text)
 DOCKER_REGISTRY=${ACCT_NO}.dkr.ecr.${REGION}.amazonaws.com
+PROJECT_NAME=DONOTDELETE-PurgeAWS-$(date "+%Y%m%d-%H%M")
+
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCT_NO}.dkr.ecr.${REGION}.amazonaws.com
 # REPO_URI=$(aws ecr create-repository --repository-name s3locust --region ${REGION} --query 'repository.repositoryUri' --output text)
 # aws ecr delete-repository --force --repository-name purge-aws
 
 # using the local Docker context build and push the images
-docker context use $LOCAL_DOCKER_CONTEXT
+# docker context use $LOCAL_DOCKER_CONTEXT
 
 # create any needed repositories
-for service in $(grep 'image: \${DOCKER_REGISTRY}' docker-compose.yaml | sed -e 's/^.*\///')
-do
-    docker buildx build --platform linux/amd64 -f ./${service}.dockerfile -t "$DOCKER_REGISTRY/$service" .
-    docker buildx build --platform linux/arm64 -f ./${service}.dockerfile -t "$DOCKER_REGISTRY/$service" .
-    aws ecr create-repository --repository-name "$service" --region ${REGION}
-done
+# for service in $(grep 'image: \${DOCKER_REGISTRY}' docker-compose.yaml | sed -e 's/^.*\///')
+# do
+#     docker buildx build --platform linux/amd64 -f ./${service}.dockerfile -t "$DOCKER_REGISTRY/$service" .
+#     docker buildx build --platform linux/arm64 -f ./${service}.dockerfile -t "$DOCKER_REGISTRY/$service" .
+#     aws ecr create-repository --repository-name "$service" --region ${REGION}
+# done
 
 # docker compose build can't be used here because the container is built on an M1 Macbook pro which produces an ARM container image
 # docker compose cannot / will not then tell Fargate to use Graviton to run the image, hence the below builds an X86 image for Fargate
-docker compose push
+# docker compose push
 
 # using the AWS ECS context run the image
 docker context use $ECS_DOCKER_CONTEXT
-docker compose --project-name DONOTDELETE-PurgeAWS up
+docker compose --project-name $PROJECT_NAME up
 
 # restore the original Docker context
 docker context use $ORIG_DOCKER_CONTEXT
