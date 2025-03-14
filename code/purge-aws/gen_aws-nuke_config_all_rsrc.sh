c92848c085c6aa2666bfe75c31adbbbaaf9e0485
Jason Barto <jason.p.barto@gmail.com>
2025-03-14 11:46:59 +0000

    updated conf file generation to be inline with new ekristen version of aws-nuke

--- code/purge-aws/gen_aws-nuke_config_all_rsrc.sh
+++ code/purge-aws/gen_aws-nuke_config_all_rsrc.sh
@@ -5,11 +5,11 @@ set -e
 CONF_FILE=/tmp/aws-nuke_all.yml
 REGIONS=$(aws ec2 describe-regions --region us-east-1 --query 'Regions[?RegionOptState!=`DISABLED`].RegionName' --output text)
 TARGET_ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text)
-RSRC_TYPES=$(aws-nuke resource-types | tr -d '\r')
+# RSRC_TYPES=$(aws-nuke resource-types | cut -d' ' -f1 | tr -d '\r')
 
 cat > $CONF_FILE <<EOF
 ---
-account-blocklist:
+blocklist:
   - "012345678901"
 
 settings:
@@ -30,10 +30,10 @@ settings:
 accounts:
   "${TARGET_ACCOUNT}":
     presets:
-    - isengard
+    - defaults
+    - organization
     - tagged_dnd
     - named_dnd
-    - okta
 
 regions:
 EOF
@@ -53,50 +53,118 @@ cat >>$CONF_FILE <<EOF
 
 resource-types:
   excludes:
-  - S3Object
+    - S3Object # Excluded because S3 bucket removal handles removing all S3Objects
+    - ServiceCatalogTagOption # Excluded due to https://github.com/rebuy-de/aws-nuke/issues/515
+    - ServiceCatalogTagOptionPortfolioAttachment # Excluded due to https://github.com/rebuy-de/aws-nuke/issues/515
+    - FMSNotificationChannel # Excluded because it's not available
+    - FMSPolicy # Excluded because it's not available
+    - MachineLearningMLModel # Excluded due to ML being unavailable
+    - MachineLearningDataSource # Excluded due to ML being unavailable
+    - MachineLearningBranchPrediction # Excluded due to ML being unavailable
+    - MachineLearningEvaluation # Excluded due to ML being unavailable
+    - RoboMakerDeploymentJob # Deprecated Service
+    - RoboMakerFleet # Deprecated Service
+    - RoboMakerRobot # Deprecated Service
+    - RoboMakerSimulationJob
+    - RoboMakerRobotApplication
+    - RoboMakerSimulationApplication
+    - OpsWorksApp # Deprecated service
+    - OpsWorksInstance # Deprecated service
+    - OpsWorksLayer # Deprecated service
+    - OpsWorksUserProfile # Deprecated service
+    - OpsWorksCMBackup # Deprecated service
+    - OpsWorksCMServer # Deprecated service
+    - OpsWorksCMServerState # Deprecated service
+    - CodeStarProject # Deprecated service
+    - CodeStarConnection # Deprecated service
+    - CodeStarNotification # Deprecated service
+    - Cloud9Environment # Deprecated service
+    - CloudSearchDomain # Deprecated service
+    - RedshiftServerlessSnapshot # Deprecated service
+    - RedshiftServerlessNamespace # Deprecated service
+    - RedshiftServerlessWorkgroup # Deprecated service
+  includes:
+    - EC2Instance
+    - RDSDBCluster
+    - RDSInstance
+    - DynamoDBTable
+    - EKSCluster 
+    - LambdaFunction 
 
 presets:
-  okta:
+  organization:
     filters:
       IAMSAMLProvider:
-      - type: contains
-        value: "okta"
+        - property: ARN
+          type: contains
+          value: "AWSSSO"
+      IAMRole:
+        - property: Name
+          type: contains
+          value: "OrganizationAccountAccessRole"
       IAMRolePolicyAttachment:
-      - "SAML-admin-role -> AdministratorAccess"
-      - "SAML-readonly-role -> ReadOnlyAccess"
-  isengard:
-    filters:
-EOF
-for t in ${RSRC_TYPES}:
-do
-    echo "      $t:" >> $CONF_FILE
-    echo '      - type: regex' >>$CONF_FILE
-    echo '        value: "[Ii]sengard"' >>$CONF_FILE
-done
+        - property: RoleName
+          value: "OrganizationAccountAccessRole"
 
-# generate a preset to protect resources with a DO-NOT-DELETE tag set
-cat >>$CONF_FILE <<EOF
-  tagged_dnd:
+  defaults:
     filters:
-EOF
-for t in ${RSRC_TYPES}:
-do
-    echo "      $t:" >> $CONF_FILE
-    echo '      - property: "tag:DO-NOT-DELETE"' >>$CONF_FILE
-    echo '        type: regex' >>$CONF_FILE
-    echo '        value: ".+"' >>$CONF_FILE
-done
+      EC2Subnet:
+        - property: DefaultVPC
+          value: "true"
+      EC2DefaultSecurityGroupRule:
+        - property: DefaultVPC
+          value: "true"
+      EC2DHCPOption:
+        - property: DefaultVPC
+          value: "true"
+      EC2VPC:
+        - property: IsDefault
+          value: "true"
+      EC2InternetGateway:
+        - property: DefaultVPC
+          value: "true"
+      EC2InternetGatewayAttachment:
+        - property: DefaultVPC
+          value: "true"
 
-# generate a preset to protect resources with 'DO-NOT-DELETE' in the name
-cat >>$CONF_FILE <<EOF
-  named_dnd:
+  tagged_dnd: # prevent deletion of anything tagged with a tag named 'DO-NOT-DELETE'
     filters:
+      __global__:
+        - property: tag:DO-NOT-DELETE
+          type: regex
+          value: ".*(?i)DO-NOT-DELETE.*"
+
+  named_dnd: # prevent deletion of anything named 'DO-NOT-DELETE'
+    filters:
+      __global__:
+        - property: Name
+          type: regex
+          value: ".*(?i)DO-NOT-DELETE.*"
 EOF
-for t in ${RSRC_TYPES}:
-do
-    echo "      $t:" >>$CONF_FILE
-    echo '      - type: regex' >>$CONF_FILE
-    echo '        value: "(?i)DO-NOT-DELETE"' >>$CONF_FILE
-done
+
+# # generate a preset to protect resources with a DO-NOT-DELETE tag set
+# cat >>$CONF_FILE <<EOF
+#   tagged_dnd: # prevent deletion of anything tagged 'DO-NOT-DELETE'
+#     filters:
+# EOF
+# while IFS= read -r t; 
+# do
+#     echo "      $t:" >> $CONF_FILE
+#     echo '      - property: "tag:DO-NOT-DELETE"' >>$CONF_FILE
+#     echo '        type: regex' >>$CONF_FILE
+#     echo '        value: ".+"' >>$CONF_FILE
+# done <<< "${RSRC_TYPES}"
+
+# # generate a preset to protect resources with 'DO-NOT-DELETE' in the name
+# cat >>$CONF_FILE <<EOF
+#   named_dnd: # prevent deletion of anything named 'DO-NOT-DELETE'
+#     filters:
+# EOF
+# while IFS= read -r t; 
+# do
+#     echo "      $t:" >>$CONF_FILE
+#     echo '      - type: regex' >>$CONF_FILE
+#     echo '        value: "(?i)DO-NOT-DELETE"' >>$CONF_FILE
+# done <<< "${RSRC_TYPES}"
 
 cat $CONF_FILE
