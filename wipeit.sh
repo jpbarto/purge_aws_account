f9c5d3de64b555402e368c345f3cff0fcfab8d5a
Jason Barto <jason.p.barto@gmail.com>
2025-04-26 02:03:36 +0100

    gave the wipeit script the ability to detect a suitable subnet

--- wipeit.sh
+++ wipeit.sh
@@ -6,30 +6,24 @@ REGION="${AWS_DEFAULT_REGION:-us-east-2}"
 ACCT_NO=$(aws sts get-caller-identity --query 'Account' --output text)
 
 # This script will 
-# - build a docker container using the Dockerfile, 
+# - use the purge container image hosted on GitHub
 # - create an ECS Fargate cluster on AWS,
 # - push the container to an ECR repository,
 # - create and run a task on the cluster to wipe the account
 #
 # This script assumes that Docker and Terraform are installed locally.
 
-LOCAL_IMAGE_TAG='local/wipeit:latest'
-docker build . -t ${LOCAL_IMAGE_TAG}
-
 export AWS_DEFAULT_REGION=${REGION}
+cd deploy/terraform
 terraform init
 terraform apply -auto-approve
-ECR_URL=$(terraform output -raw ecr_url)
-
-aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCT_NO}.dkr.ecr.${REGION}.amazonaws.com
-docker tag ${LOCAL_IMAGE_TAG} "${ECR_URL}:latest"
-docker push "${ECR_URL}:latest"
 
 PURGE_CLUSTER=$(terraform output -raw purge_cluster)
 TASK_ARN=$(terraform output -raw task_arn)
+SUBNET_ID=$(aws ec2 describe-subnets --filters 'Name=availability-zone,Values='$REGION'a' 'Name=default-for-az,Values=true' --query 'Subnets[0].SubnetId' --output text)
 aws ecs run-task \
     --task-definition ${TASK_ARN} \
     --region ${REGION} \
     --cluster ${PURGE_CLUSTER} \
     --launch-type FARGATE \
-    --network-configuration 'awsvpcConfiguration={subnets=["subnet-b56f47ff"],assignPublicIp="ENABLED"}'
+    --network-configuration 'awsvpcConfiguration={subnets=["'$SUBNET_ID'"],assignPublicIp="ENABLED"}'
